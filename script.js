cdaee1e2a78cc7e8e09415ad2e52f5c63a350b3b
Yoon Me Me <yoonmeme.engr@gmail.com>
2024-11-23 12:39:54 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const controlAside = document.querySelector(".control-aside");
+
+controlAside.addEventListener("click", () => {
+    const wrapper = document.querySelector(".wrapper").classList.toggle("show");
+})
